24e94fcfe86e94683aa6be8bfe2288b1a0fc4656
Claudio Francisco das Neves <cfneguacu@uni9.edu.br>
2022-10-29 14:19:29 -0300

    Array Odd Even Ruby

--- lib/arrays.rb
+++ lib/arrays.rb
@@ -1,7 +1,11 @@
 class Arrays
   def self.converte_impares_por(lista, numero)
+    listateste = lista.filter{|a| a%2==1}
+    listaimpares = [listateste, listateste.map{|x| x * numero }]
   end
 
   def self.converte_pares_por(lista, numero)
+    listateste = lista.filter{|a| a%2==0}
+    listapares = [listateste, listateste.map{|x| x * numero }]
   end
 end
